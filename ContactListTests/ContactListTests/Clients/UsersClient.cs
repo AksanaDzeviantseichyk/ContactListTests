3b036ea5a308e71214869dd04a0ab80eccf5c77c
Aksana Dzeviantseichyk <a.dzeviantseichyk@gmail.com>
2024-03-18 23:55:06 +0100

    users client

--- ContactListTests/ContactListTests/Clients/UsersClient.cs
+++ ContactListTests/ContactListTests/Clients/UsersClient.cs
@@ -1,6 +1,20 @@
-﻿namespace ContactList.Core.Clients
+﻿using ContactList.Core.HTTP;
+using Microsoft.Extensions.Configuration;
+
+namespace ContactList.Core.Clients
 {
     public class UsersClient
     {
+        private readonly string _url;
+
+        private readonly HtpClientCustom _client;
+
+        public UsersClient(IConfiguration configuration,
+            HtpClientCustom client)
+        {
+            _url = configuration["Url:userService"];
+            _client = client;
+        }
+
     }
 }

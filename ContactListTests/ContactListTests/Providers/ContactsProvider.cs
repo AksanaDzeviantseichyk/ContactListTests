360ceac8719c1ebd0617e9a3c1d35aa1add3771d
Aksana Dzeviantseichyk <a.dzeviantseichyk@gmail.com>
2024-03-21 11:45:32 +0100

    construcrors in providers

--- ContactListTests/ContactListTests/Providers/ContactsProvider.cs
+++ ContactListTests/ContactListTests/Providers/ContactsProvider.cs
@@ -1,6 +1,17 @@
-﻿namespace ContactList.Core.Providers
+﻿using ContactList.Core.Clients;
+using ContactList.Core.Utils;
+
+namespace ContactList.Core.Providers
 {
     public class ContactsProvider
     {
+        private readonly ContactsClient _contactClient;
+        private readonly ContactsGenerator _contactGenerator;
+        public ContactsProvider(ContactsClient client,
+           ContactsGenerator generator)
+        {
+            _contactClient = client;
+            _contactGenerator = generator;
+        }
     }
 }
--- ContactListTests/ContactListTests/Providers/UsersProvider.cs
+++ ContactListTests/ContactListTests/Providers/UsersProvider.cs
@@ -1,6 +1,17 @@
-﻿namespace ContactList.Core.Providers
+﻿using ContactList.Core.Clients;
+using ContactList.Core.Utils;
+
+namespace ContactList.Core.Providers
 {
     public class UsersProvider
     {
+        private readonly UsersClient _userClient;
+        private readonly UsersGenerator _userGenerator;
+        public UsersProvider(UsersClient client,
+           UsersGenerator generator)
+        {
+            _userClient = client;
+            _userGenerator = generator;
+        }
     }
 }
